a8bf1c408f1371face27b1084064546cd814915a
Matheus Menezes <droids-labs-0@icloud.com>
2024-01-10 18:37:06 -0300

    Refactored LatestUiState

--- app/src/main/java/br/com/mathsemilio/simpleapodbrowser/feature/latest/ui/model/LatestUiState.kt
+++ app/src/main/java/br/com/mathsemilio/simpleapodbrowser/feature/latest/ui/model/LatestUiState.kt
@@ -2,9 +2,10 @@ package br.com.mathsemilio.simpleapodbrowser.feature.latest.ui.model
 
 import br.com.mathsemilio.simpleapodbrowser.domain.model.Apod
 import br.com.mathsemilio.simpleapodbrowser.feature.latest.ui.view.list.PeriodFilterOption
+import br.com.mathsemilio.simpleapodbrowser.feature.latest.ui.view.list.PeriodFilterOption.LAST_WEEK
 
 data class LatestUiState(
-    val periodSelected: PeriodFilterOption = PeriodFilterOption.LAST_WEEK,
+    val periodSelected: PeriodFilterOption = LAST_WEEK,
     val loading: Boolean = true,
     val displayError: Boolean = false,
     val apods: List<Apod> = emptyList()
