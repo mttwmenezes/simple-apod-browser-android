1bb277f6047f50d1714b807f81db5bb2f4f612a2
Matheus Menezes <droids-labs-0@icloud.com>
2024-01-10 16:56:51 -0300

    Fixed year on ImageResource.kt license header

--- app/src/main/java/br/com/mathsemilio/simpleapodbrowser/feature/common/ImageResource.kt
+++ app/src/main/java/br/com/mathsemilio/simpleapodbrowser/feature/common/ImageResource.kt
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Matheus Menezes
+Copyright 2024 Matheus Menezes
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
