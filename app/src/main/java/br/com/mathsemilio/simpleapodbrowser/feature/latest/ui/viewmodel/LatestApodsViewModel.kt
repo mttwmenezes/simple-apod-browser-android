5f1a40cbfd46137b18bc6566505035d967e0f97e
Matheus Menezes <droids-labs-0@icloud.com>
2024-01-10 18:46:23 -0300

    Refactored LatestApodsViewModel
    Refactored LatestApodsViewModel
    Added license header for LatestUiState and ApplicationModule
    Removed some of the default paramater values from LatestUiState

--- app/src/main/java/br/com/mathsemilio/simpleapodbrowser/feature/latest/ui/viewmodel/LatestApodsViewModel.kt
+++ app/src/main/java/br/com/mathsemilio/simpleapodbrowser/feature/latest/ui/viewmodel/LatestApodsViewModel.kt
@@ -1,3 +1,19 @@
+/*
+Copyright 2024 Matheus Menezes
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package br.com.mathsemilio.simpleapodbrowser.feature.latest.ui.viewmodel
 
 import androidx.lifecycle.LiveData
@@ -27,14 +43,14 @@ class LatestApodsViewModel @Inject constructor(
     }
 
     fun retrieveFrom(periodSelected: PeriodFilterOption) {
-        val loadingState = LatestUiState(periodSelected, loading = true)
+        val loadingState = LatestUiState(periodSelected, loading = true, displayError = false)
         val errorState = LatestUiState(periodSelected, loading = false, displayError = true)
 
         _uiState.postValue(loadingState)
 
         viewModelScope.launch {
             repository.retrieveFromDateRange(formattedStartDate(periodSelected)).also {
-                if (it.isEmpty()) {
+                if (it.isNotEmpty()) {
                     val successState = LatestUiState(
                         periodSelected,
                         loading = false,
